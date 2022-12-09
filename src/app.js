84ad67ba49bac8baa21715b58a81e4de3ab3b8ac
SberIrk <114170642+SberIrk@users.noreply.github.com>
2022-12-10 04:25:06 +0800

    init menu test block

--- src/app.js
+++ src/app.js
@@ -1,2 +1,14 @@
 import './styles.css'
+import {ContextMenu} from "./menu.js"
+import {TestModule} from "./modules/test.module.js";
 
+
+function init(){
+    const menu = new ContextMenu('.menu');
+    for (let i = 1; i < 6; i++){
+        console.log(i);
+        menu.add(new TestModule('test'+i));
+    }
+}
+
+init();
