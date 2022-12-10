84feb9342d54a1ec3f1b5eec1f0259e213ee0aea
NakhimoVV <tj1993@bk.ru>
2022-12-10 23:08:51 +0300

    fix custom message module

--- src/app.js
+++ src/app.js
@@ -1,16 +1,15 @@
 import './styles.css'
-import {ContextMenu} from "./menu.js"
-import {ShapeModule} from "@/modules/shape.module";
-import {SoundModule} from "@/modules/sound.module";
-import {CustomMessage} from "@/modules/custom.message";
-import {BackgroundModule} from "@/modules/background.module.js";
-import {dataMessage} from "@/dataMessage";
+import { ContextMenu } from "./menu.js"
+import { ShapeModule } from "@/modules/shape.module";
+import { SoundModule } from "@/modules/sound.module";
+import { CustomMessage } from "@/modules/custom.message";
+import { BackgroundModule } from "@/modules/background.module.js";
 
-function init(){
+function init() {
     const menu = new ContextMenu('.menu');
     menu.add(new SoundModule('soundModule'));
     menu.add(new ShapeModule('shapeModule'));
-    menu.add(new CustomMessage('CustomMessage',dataMessage));
+    menu.add(new CustomMessage('CustomMessage'));
     menu.add(new BackgroundModule('BackgroundModule'));
 }
 
